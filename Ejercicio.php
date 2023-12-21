ec15ac91490e4cdaa02eba4490831a8ecf0aadfa
Marta Villalba <marta.villalba.orta@gmail.com>
2023-12-21 10:13:21 +0100

    Ejercicio empezado, falta terminarlo

---
+++ Ejercicio.php
@@ -0,0 +1,25 @@
+<?php
+
+$lista1 = array(25, 12, 23);
+$num1=1;
+$num2 = 3;
+$lista2 = array(31,12,23);
+
+function girarOrdenArray ($lista1, $num1){
+    
+    $resultado = [];
+
+    //Coger la posicion 1, y la pasa a ser la posicion 0 del nuevo array
+    for ($i = $num1; $i <= $num1; $i++) {
+        $resultado[] = $lista1[$i];
+    }
+
+    //pasar los otros restantes al nuevo array
+
+    
+
+    return $resultado;
+}
+
+
+?>
---
+++ Kata.php
@@ -0,0 +1,17 @@
+Fes una funció que, donat un array d’elements i un nombre, ens retorni l'array rotant 
+cap a l’esquerra els elements de l'array tantes posicions com indiquem al nombre.
+
+Per exemple:
+
+Input
+
+[25,12,23] 1
+[31,12,23] 3
+[“Bones”,” Festes”,”!”] 2
+Output
+
+[12,23,25]
+[31,12,23]
+["!","Bones","Festes"]
+
+
